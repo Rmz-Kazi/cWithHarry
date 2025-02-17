285949f6ff1ded46dd9815ff20faed6f0eadb382
Rmz-Kazi <ramiz.aslam.kazi@hotmail.com>
2025-02-17 21:54:35 +0500

    Product of 2 numbers (Hardcoded)

---
+++ practiceSet1/area1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+  int one = 2;
+  int two = 3;
+  int result = one * two;
+
+  printf("The product of the above two numbers is: %d\n", result);
+
+  return 0;
+}
